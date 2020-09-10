b7a281e8d6e83b8ff516add6a3911fd2ff68cf76
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-09-10 12:25:51 +0300

    Remove console.log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("open\n", req.session)
   res.send('hello, this is without an engine');
 });
 
