9089382602df9193636f7c9b87dc0286d3cef403
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-10-04 11:33:33 +0300

    Add sequelize related items in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,53 +1,69 @@
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import cors from 'cors';
-import expressWs from 'express-ws';
-import session from 'express-session';
-import redis from './redis';
-import redisStore from 'connect-redis';
+require("dotenv").config();
+import express from "express";
+import bodyParser from "body-parser";
+import createError from "http-errors";
+import path from "path";
+import morgan from "morgan";
+import cors from "cors";
+import expressWs from "express-ws";
+import session from "express-session";
+// import redis from "./redis";
+import db from "./models";
+import { PRIVILEGE_LEVELS } from "./constants";
 
-redis.setNewPlayer({
-    id: 'utku',
-    name: 'banana',
-})
+const { NODE_ENV } = process.env;
+const { app } = expressWs(express());
+const Role = db.Role;
 
-const { app, getWss, applyTo } = expressWs(express());
-const redisStoreSession = redisStore(session);
+// Routes need to come after the websocket wrapper implementation
+import indexRouter from "./routes/index";
+import websocketRouter from "./routes/websocket";
+import restRouter from "./routes/rest";
 
-import indexRouter from './routes/index';
-import restRouter from './routes/rest';
-import websocketRouter from './routes/websocket';
+if (NODE_ENV === "development") {
+  db.sequelize.sync({ force: true }).then(() => {
+    initial();
+  });
+
+  function initial() {
+    Role.create({
+      id: 1,
+      name: PRIVILEGE_LEVELS.user,
+    });
+
+    Role.create({
+      id: 2,
+      name: PRIVILEGE_LEVELS.moderator,
+    });
+
+    Role.create({
+      id: 3,
+      name: PRIVILEGE_LEVELS.admin,
+    });
+  }
+} else {
+  db.sequelize.sync();
+}
 
 app.use(cors());
-app.use(logger("dev"));
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const RedisStore = require("connect-redis")(session);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-    secret: ['veryimportantsecret'],  
-    name: "secretname",
-    cookie: {
-        httpOnly: false,
-        secure: false,
-        sameSite: false,
-        maxAge: 600000 // Time is in miliseconds
-    },
-    store: new redisStoreSession({ host: 'redis', port: 6379, client: redis.getClient()}),
-    saveUninitialized: false,
-    resave: false,
-}));
 
+// Routes
 app.use("/", indexRouter);
-app.use("/rest", restRouter);
 app.use("/ws", websocketRouter);
+app.use("/rest", restRouter);
 
-// catch 404 and forward to error handler
+// 404 catch and forward
 app.use((req, res, next) => {
-    next(createError(404));
+  next(createError(404));
 });
 // // error handler
 // app.use((err, req, res, next) => {
@@ -60,10 +76,9 @@ app.use((req, res, next) => {
 //     res.render("error");
 // });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server is listening on: ${PORT}`);
+  console.log(`Server is listening on: ${PORT}`);
 });
-
 
 export default app;
