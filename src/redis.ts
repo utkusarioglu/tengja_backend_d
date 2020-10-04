26fa893c3f90536c08eee6c4f385e2487d8aa4e8
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-10-04 11:22:57 +0300

    Cosmetic changes to Redis

--- src/redis.ts
+++ src/redis.ts
@@ -1,30 +1,35 @@
-import redis from 'redis';
-import { promisify } from 'util';
+import redis from "redis";
+import { promisify } from "util";
 
-
-export const client = redis.createClient({host: 'redis'});
+export const client = redis.createClient({ host: "redis" });
 
 class Redis {
+  private client = redis.createClient({ host: "redis" });
+  private getAsync = promisify(client.get).bind(client);
+  private setAsync = promisify(client.set).bind(client);
 
-    private client = redis.createClient({host: 'redis'});
-    private getAsync = promisify(client.get).bind(client);
-    private setAsync = promisify(client.set).bind(client);
+  constructor() {
+    this.client.on("error", (err) => {
+      console.log("redis error", err);
+    });
+  }
 
-    public getClient(): redis.RedisClient {
-        return client;
-    }
-    
-    public setNewPlayer(player: any): void {
-        this.setAsync(`activePlayer.${player.id}`, JSON.stringify(player)).then((res) => {
-            console.warn('player saved')
-        })
-    }
+  public getClient(): redis.RedisClient {
+    console.log("calling redis client");
+    return this.client;
+  }
 
-    public getPlayer(playerId: string): Promise<any> {
-        return this.getAsync(`activePlayer.${playerId}`)
-    }
+  public setNewPlayer(player: any): void {
+    this.setAsync(`activePlayer:${player.id}`, JSON.stringify(player)).then(
+      (res) => {
+        process.env.NODE_ENV === "development" && console.log("Player saved");
+      }
+    );
+  }
 
-
+  public getPlayer(playerId: string): Promise<any> {
+    return this.getAsync(`activePlayer.${playerId}`);
+  }
 }
 
 export default new Redis();
